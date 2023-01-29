8289544f17d0baa2c7da222ca33a2d3915f6de98
unknown <carlosruanaraujo789@gmail.com>
2023-01-29 01:46:50 -0300

    modificando animaçao de loading  - em desenvolvimento

--- PokeDex/src/App.js
+++ PokeDex/src/App.js
@@ -1,55 +1,66 @@
-
-import { useEffect, useRef, useState } from 'react';
-import './App.css';
+import { useEffect,  useState } from "react";
+import "./App.css";
 
 function App() {
-  const[data,setdata] = useState()
- 
-  useEffect(() =>{
-    getdata()
-  },[])
+  const [data, setdata] = useState();
 
-  async function getdata(){
-   await fetch("https://pokeapi.co/api/v2/pokemon?limit=260",{
-      method: "get"
-    }).then(res => res.json())
-    
-    .then(res =>  res.results.map((e) => e.url))
-    .then(urls => Promise.all(urls.map(url => fetch(url))))
-    .then(responses =>Promise.all(responses.map(res => res.json())))
-    .then(responsesDATA => responsesDATA.map(res => res.sprites.front_default))
-    .then(images => setdata(images))
+  useEffect(() => {
+    getdata();
+  }, []);
 
-   
+  async function getdata() {
+    await fetch("https://pokeapi.co/api/v2/pokemon?limit=10", {
+      method: "get",
+    })
+      .then((res) => res.json())
+
+      .then((res) => res.results.map((e) => e.url))
+      .then((urls) => Promise.all(urls.map((url) => fetch(url))))
+      .then((responses) => Promise.all(responses.map((res) => res.json())))
+      .then((data) => setdata(data));
   }
   return (
-   <div>
+    <div>
+      {console.log(data)}
+      <nav className="navbar navbar-expand-md   bg-dark">
+        <button
+          data-bs-toggle="collapse"
+          data-bs-target="#teste"
+          className="navbar-toggler bg-white"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-      <nav className='navbar navbar-expand-md   bg-dark'>
-    
-<button data-bs-toggle="collapse" data-bs-target="#teste" className='navbar-toggler bg-white'>
-  <span className='navbar-toggler-icon'></span>
-</button>
-
-
-      
-        <ul id="teste" className='navbar-nav navbar-collapse collapse '>
-          <li className='nav-item '><a className='nav-link text-white'>Inicio</a></li>
-          <li className='nav-item'><a className='nav-link text-white'>Inicio</a></li>
-          <li className='nav-item'><a className='nav-link text-white'>Inicio</a></li>
-
+        <ul id="teste" className="navbar-nav navbar-collapse collapse ">
+          <li className="nav-item ">
+            <a href="*" className="nav-link text-white">Inicio</a>
+          </li>
+          <li className="nav-item">
+            <a href="*" className="nav-link text-white">Inicio</a>
+          </li>
+          <li className="nav-item">
+            <a href="*" className="nav-link text-white">Inicio</a>
+          </li>
         </ul>
-        
       </nav>
-{data?
-  data.map((image) => {
-   return( <img key={image} src={image}></img>)
-  })
-  :
-  "ola"
-}
- 
-      </div>
+      {data ? (
+        data.map((data) => {
+          return (
+            <img
+              alt=""
+              key={data.sprites.front_default}
+              src={data.sprites.front_default}
+            ></img>
+          );
+        })
+      ) : (
+        <div className="d-flex pai">
+          <div className="load"></div>
+          <div className="load"></div>
+          <div className="load"></div>
+        </div>
+      )}
+    </div>
   );
 }
 
