ec0c3d47e08be26a2151380ddc13536472659332
unknown <carlosruanaraujo789@gmail.com>
2023-01-30 20:56:33 -0300

    deploy

--- PokeDex/src/App.js
+++ PokeDex/src/App.js
@@ -22,7 +22,7 @@ const onClickPokemon = (data,color) =>{
 setdataPokemon({...dataPokemon,data : data,color: color});
 }
   async function getdata() {
-    await fetch("https://pokeapi.co/api/v2/pokemon?limit=300", {
+    await fetch("https://pokeapi.co/api/v2/pokemon?limit=10000", {
       method: "get",
     })
       .then((res) => res.json())
